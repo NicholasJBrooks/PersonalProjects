4720a59fb999912e347a0c0dd232370e68dc0902
NicholasJBrooks <40639560+NicholasJBrooks@users.noreply.github.com>
2019-01-14 22:46:55 -0500

    Basic setup
    Added the Transaction class, bower structure,  and initial files

---
+++ BlankFinance/BlankFinance/Models/Transaction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlankFinance.Models
+{
+    public class Transaction
+    {
+        [Required(ErrorMessage = "Please enter a valid date")]
+        public DateTime Date { get; set; }
+
+        [Required(ErrorMessage = "Please enter a transaction type")]
+        public string Type { get; set; }
+
+        [Required(ErrorMessage = "Please enter a description")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Please enter an amount")]
+        public decimal Amount { get; set; }
+
+        [Required(ErrorMessage = "Please enter a category")]
+        public string Category { get; set; }
+    }
+}
