10caf581d2b378f795212718f37a870967124883
NicholasJBrooks <40639560+NicholasJBrooks@users.noreply.github.com>
2019-01-18 21:13:29 -0500

    Add some of the initial Entity Framework files and connection string

---
+++ BlankFinance/BlankFinance/Models/EFTransactionRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BlankFinance.Models;
+using System.Threading.Tasks;
+
+namespace BlankFinance.Models
+{
+    public class EFTransactionRepository: ITransactionRepository
+    {
+        private ApplicationDBContext context;
+
+        public EFTransactionRepository(ApplicationDBContext ctx)
+        {
+            this.context = ctx;
+        }
+
+        public IQueryable<Transaction> Transactions => context.Transactions;
+
+        public Transaction DeleteTransaction(int TransactionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveTransaction(Transaction transaction)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BlankFinance/BlankFinance/Models/ITransactionRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlankFinance.Models
+{
+    public interface ITransactionRepository
+    {
+        IQueryable<Transaction> Transactions { get;  }
+
+        void SaveTransaction(Transaction transaction);
+
+        Transaction DeleteTransaction(int TransactionId); 
+    }
+}
