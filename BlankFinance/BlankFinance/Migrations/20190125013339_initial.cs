0d0ed35ab2cf2d43115c863c5ef83a5ebe772c6e
NicholasJBrooks <40639560+NicholasJBrooks@users.noreply.github.com>
2019-01-27 00:23:10 -0500

    Added initial migration and CSVConverter.

---
+++ BlankFinance/BlankFinance/Migrations/20190125013339_initial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlankFinance.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    TransactionId = table.Column<Guid>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Type = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(nullable: false),
+                    Amount = table.Column<decimal>(nullable: false),
+                    Category = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.TransactionId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transactions");
+        }
+    }
+}
--- BlankFinance/BlankFinance/Models/Transaction.cs
+++ BlankFinance/BlankFinance/Models/Transaction.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BlankFinance.Models
 {
     public class Transaction
     {
-        Guid TransactionId { get; set; }
+        public Guid TransactionId { get; set; }
 
         [Required(ErrorMessage = "Please enter a valid date")]
         public DateTime Date { get; set; }
