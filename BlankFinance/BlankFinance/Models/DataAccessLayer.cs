2de397a21d26dd5574285295c6f7e777a0aeb714
NicholasJBrooks <40639560+NicholasJBrooks@users.noreply.github.com>
2019-11-10 12:29:31 -0500

    Check in to work on laptop.

--- BlankFinance/BlankFinance/Models/DataAccessLayer.cs
+++ BlankFinance/BlankFinance/Models/DataAccessLayer.cs
@@ -25,19 +25,23 @@ namespace BlankFinance.Models.Interfaces
             using (_sqlConnectionForBlankFinance = new SqlConnection(Configuration["ConnectionStrings:BFTransactions:ConnectionString"]))
             {
                 _sqlConnectionForBlankFinance.Open();
+                SqlString xml;
 
                 //Create a command to execute 
                 SqlCommand _sqlCommand = new SqlCommand();
-                _sqlCommand.CommandText = "SELECT * FROM Budget";
+                _sqlCommand.CommandText = "SELECT * FROM Budgets";
                 _sqlCommand.CommandType = CommandType.Text;
                 _sqlCommand.Connection = _sqlConnectionForBlankFinance;
 
                 /* Data Reader Demo */
                 //Execute the command and store the data result-set into a data reader 
                 SqlDataReader _sqlReader = _sqlCommand.ExecuteReader();
-                //Read each record from data reader at a time  
-                SqlString xml = _sqlReader.GetString(0);
 
+                //Read each record from data reader at a time  
+                if (_sqlReader.HasRows)
+                {
+                     xml = _sqlReader.GetString(0);
+                }
                 if (xml != null)
                 {
                      budget.BudgetXML = xml.Value;
